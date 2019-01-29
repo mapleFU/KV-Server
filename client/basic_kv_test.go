e5a7f40367f02c86a328a06a1eab3be0b79b0d78
mapleFU <1506118561@qq.com>
2019-01-29 15:58:43 +0800

    [ADD] Impl and finish basic logic for Get Set Delete Scan without considering thread-safe

--- client/basic_kv_test.go
+++ client/basic_kv_test.go
@@ -7,35 +7,112 @@ import (
 	"golang.org/x/net/context"
 	"testing"
 	"strings"
+	"strconv"
 )
 
-func TestBasicKV(t *testing.T) {
+const (
+	rpcAddress = "localhost:50001"
+	testKey = "test-key"
+)
+
+func TestBasicKVSetAndGet(t *testing.T) {
 	// Set up a connection to the server.
-	conn, err := grpc.Dial("localhost:50001", grpc.WithInsecure())
+	conn, err := grpc.Dial(rpcAddress, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewKVServicerClient(conn)
-
+	testValue := []byte(testKey)
 	// Contact the server and print out its response.
 	//name := defaultName
 	//if len(os.Args) > 1 {
 	//	name = os.Args[1]
 	//}
 
-	_, err = c.Set(context.Background(), &pb.KVPair{Key:&pb.Key{Key:"hello"},
-											Value:&pb.Value{Values: []byte("hello")}})
+	// set testkey and get testkey
+	_, err = c.Set(context.Background(), &pb.KVPair{Key:&pb.Key{Key:testKey},
+											Value:&pb.Value{Values: testValue}})
 
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
 
-	getResp, err := c.Get(context.Background(), &pb.Key{Key:"hello"})
+	getResp, err := c.Get(context.Background(), &pb.Key{Key:testKey})
 	if err != nil {
 		log.Fatalln(err)
 	}
-	if strings.Compare(string(getResp.Values), "hello") != 0 {
-		t.Fatalf("error, set %s but got %s", "hello", string(getResp.Values))
+	if strings.Compare(string(getResp.Values), string(testValue)) != 0 {
+		t.Fatalf("error, set %s but got %s", testKey, string(getResp.Values))
 	}
+}
+
+func TestBasicKVGetNil(t* testing.T)  {
+	conn, err := grpc.Dial(rpcAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewKVServicerClient(conn)
+	resp, err := c.Get(context.Background(), &pb.Key{Key:"non-exists"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.Values) > 0 {
+		t.Fatalf("Length of resp.Values > 0, get not nil.")
+	}
+}
+
+func TestBasicKVDelete(t *testing.T)  {
+	conn, err := grpc.Dial(rpcAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewKVServicerClient(conn)
+	testValue := []byte("non-exists")
+	c.Set(context.Background(), &pb.KVPair{Key:&pb.Key{Key:"non-exists"}, Value:&pb.Value{Values:testValue}})
+	c.Delete(context.Background(), &pb.Key{Key:"non-exists"})
+
+	resp, err := c.Get(context.Background(), &pb.Key{Key:"non-exists"})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if len(resp.Values) > 0 {
+		t.Fatalf("Length of resp.Values > 0, get not nil.")
+	}
+}
+
+func TestBasicKVScan(t *testing.T)  {
+	conn, err := grpc.Dial(rpcAddress, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewKVServicerClient(conn)
+
+	for i := 0; i <= 1000; i++ {
+		c.Set(context.Background(), &pb.KVPair{
+			Key: &pb.Key{Key:"Test" + strconv.Itoa(i)},
+			Value: &pb.Value{Values: []byte(strconv.Itoa(i))},
+		})
+	}
+
+	scan, err := c.Scan(context.Background(), &pb.ScanArgs{
+		Match: &pb.Key{Key:"Test.*9$"},
+		Count: 1000,
+		Cursor: 0,
+	})
+	for _, v := range  scan.Values {
+
+		i, err := strconv.Atoi(string(v.Values))
+		if err != nil {
+			t.Fatal(err)
+		}
+		//fmt.Println(i)
+		if i % 10 != 9 {
+			t.Fatalf("%d is not match the regex, unpass", i)
+		}
+	}
+	//fmt.Printf("Length %d\n", len(scan.Values))
 }
--- server/main.go
+++ server/main.go
@@ -7,6 +7,7 @@ import (
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/grpc"
 	"context"
+	"regexp"
 )
 
 const (
@@ -17,6 +18,12 @@ type KVService struct {
 	kvMap map[string][]byte
 }
 
+var EmptyArray []byte
+
+func init()  {
+	EmptyArray = make([]byte, 0)
+}
+
 func (s *KVService) Get(ctx context.Context, req *pb.Key) (*pb.Value, error) {
 	log.Infof("Call Get with Key(%s)", req.Key)
 	if values, ok := s.kvMap[req.Key]; ok {
@@ -24,7 +31,10 @@ func (s *KVService) Get(ctx context.Context, req *pb.Key) (*pb.Value, error) {
 			Values:values,
 		}, nil
 	} else {
-		return nil, nil
+		//log.Println("Send " + string(EmptyArray))
+		return &pb.Value{
+			Values: EmptyArray,
+		}, nil
 	}
 }
 
@@ -35,12 +45,35 @@ func (s *KVService) Set(ctx context.Context, req *pb.KVPair) (*pb.OperationOK, e
 }
 
 func (s *KVService) Delete(ctx context.Context, req *pb.Key) (*pb.OperationOK, error)  {
-	delete(s.kvMap, req.Key)
-	return &pb.OperationOK{Ok:true}, nil
+	var ok bool
+	if _, ok = s.kvMap[req.Key]; ok {
+		delete(s.kvMap, req.Key)
+	}
+	return &pb.OperationOK{Ok:ok}, nil
 }
 
 func (s *KVService) Scan(ctx context.Context, req *pb.ScanArgs) (*pb.Values, error) {
-	return nil, nil
+
+	values := make([]*pb.Value, 0)
+
+	reg, err := regexp.Compile(req.Match.Key)
+	if err != nil {
+		return &pb.Values{}, nil
+	}
+	index, cnt := 0, 0
+	for k, v := range s.kvMap {
+
+		if reg.MatchString(k) {
+			if int32(index) > req.Cursor && int32(cnt) < req.Count {
+				values = append(values, &pb.Value{Values:v})
+				cnt++
+			}
+			index++
+		}
+	}
+	return &pb.Values{
+		Values:values,
+	}, nil
 }
 
 func NewKVService() *KVService {
@@ -66,7 +99,11 @@ func main()  {
 
 	// Register reflection service on gRPC server.
 	reflection.Register(s)
+
+	log.Infoln("KV-Server started.")
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
+
+	log.Infoln("KV-Server end.")
 }
