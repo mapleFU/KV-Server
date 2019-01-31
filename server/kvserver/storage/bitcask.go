8d9d131363bfad975fa2068aa9c4d75d79794eb3
mapleFU <1506118561@qq.com>
2019-02-01 06:18:43 +0800

    [ADD] Use open-source bitcask to implement bitcask model

---
+++ server/kvserver/storage/bitcask.go
@@ -0,0 +1,41 @@
+package storage
+
+import (
+	"github.com/laohanlinux/bitcask"
+	log "github.com/sirupsen/logrus"
+)
+
+type Bitcask struct {
+	bc *bitcask.BitCask
+}
+
+func Open(dirName string) *Bitcask {
+	bc, err := bitcask.Open(dirName, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &Bitcask{
+		bc:bc,
+	}
+}
+
+func (bc *Bitcask) Close()  {
+	defer bc.bc.Close()
+}
+
+func (bc *Bitcask) Get(key []byte) ([]byte, error) {
+	return bc.bc.Get(key)
+}
+
+func (bc *Bitcask) Set(key []byte, value []byte) error {
+	
+	return bc.bc.Put(key, value)
+}
+
+func (bc *Bitcask) Scan()  {
+
+}
+
+func (bc *Bitcask) Delete(key []byte) error {
+	return bc.bc.Del(key)
+}
